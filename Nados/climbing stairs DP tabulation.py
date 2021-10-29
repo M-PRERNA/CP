bc2ecceca62fa02632346302c4c2b77070c21ce8
PRERNA MISHRA <mprerna802@gmail.com>
2021-10-29 22:28:48 +0530

    Create climbing stairs DP tabulation.py

---
+++ Nados/climbing stairs DP tabulation.py
@@ -0,0 +1,18 @@
+# count stairs tabulation
+def countpaths(n):
+    dp = [0]*(n+1)
+    dp[0] = 1
+    for i in range (1,n+1):
+        if i == 1:
+            dp[i] = dp[i-1]
+        elif i==2:
+            dp[i]= dp[i-1]+dp[i-2]
+        else:
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+    
+    return dp[n]
+    
+if __name__ == "__main__":
+    n = int(input())
+#     count = [0]*(n+1)
+    print(countpaths(n))
